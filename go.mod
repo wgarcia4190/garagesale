ebef080ad53386204687d7cd3bf4962c66825d3f
Wilson <garcia.m.wilson@gmail.com>
2020-09-12 12:27:40 -0400

    Adding support for postgres and docker

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/wgarcia4190/garagesale
 
 go 1.15
+
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.8.0
+)
--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -18,12 +22,21 @@ func main() {
 	defer log.Println("main: Completed")
 
 	// =========================================================================
+	// Setup Dependencies
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	// =========================================================================
 	// Start API Service
 	api := http.Server{
 		Addr:         "localhost:8000",
 		Handler:      http.HandlerFunc(GetListProducts),
-		ReadTimeout:  5 * time.Second, //nolint:gomnd
-		WriteTimeout: 5 * time.Second, //nolint:gomnd
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
 	}
 
 	// Make a channel to listen for errors coming from the listener. Use a
@@ -72,6 +85,22 @@ func main() {
 			return
 		}
 	}
+}
+
+func openDB() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", "disable")
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword("postgres", "postgres"),
+		Host:     "localhost",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
 }
 
 // Product is something we sell.
